e45c0796bc7e34e0529c48b976ce0275efafcd86
Andressa Novaes <isa.menezes@ilia.digital>
2020-12-16 08:04:44 -0300

    Ëstrutura Inicial

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['universe/native', 'universe/web', 'universe/shared/typescript-analysis'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+};
